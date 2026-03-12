85a58c903ef910210594a02429abd2d11ababb9a
Brittany Pollock <brittpollock15@gmail.com>
2026-03-12 16:58:52 -0700

    Initial Commit

---
+++ Habit Tracking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Habit Tracking
+//
+//  Created by Brittany Pollock on 3/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Habit Tracking/Habit_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackingApp.swift
+//  Habit Tracking
+//
+//  Created by Brittany Pollock on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
